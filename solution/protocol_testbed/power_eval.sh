b5c1fd027721b00398bbd4ae552afdf2d62f2c98
Tomoya Tanaka <totana@ttu.ee>
2021-08-19 01:45:52 +0300

    Update shell script to measure power consumption

--- solution/protocol_testbed/power_eval.sh
+++ solution/protocol_testbed/power_eval.sh
@@ -1,21 +1,22 @@
 outbasedir=out
-outdir=$(eval date +"%Y-%m-%d-%H:%M:%S")
-mkdir $outbasedir/$outdir
+outdir=$outbasedir/$(eval date +"%Y-%m-%d-%H:%M:%S")
+mkdir $outdir
+outdir=`realpath $outdir`
 
 if [ -e "out/current" ] ; then
   rm $outbasedir/current
 fi
 
 echo "ln -s $outdir out/current"
-ln -s $outdir out/current
+ln -s $outdir $outbasedir/current
 
-touch $outbasedir/$outdir/powerstat.log
-cat /dev/null > $outbasedir/$outdir/powerstat.log
+touch $outdir/powerstat.log
+cat /dev/null > $outdir/powerstat.log
 
 while true
 do
-  echo "powerstat -d 10 1 >> $outbasedir/$outdir/powerstat.log"
-  powerstat -d 10 1 >> $outbasedir/$outdir/powerstat.log
+  echo "powerstat -d 10 1 >> $outdir/powerstat.log"
+  powerstat -d 10 1 >> $outdir/powerstat.log
 done
 
 
