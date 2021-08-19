0b7b6122f9233e38089a61fedcdf45505ab3fa9c
Tomoya Tanaka <totana@ttu.ee>
2021-08-19 18:35:22 +0300

    Format graph

--- solution/protocol_testbed/grapher/grapher.py
+++ solution/protocol_testbed/grapher/grapher.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-import argparse, re, sys
+import argparse, re, sys, math
 import pandas as pd
-from datetime import time
+from datetime import time, timedelta
 import plotly.express as px
+import plotly.graph_objects as go
 
 DEBUG = True
 
@@ -25,81 +26,146 @@ def generate_cpu_usage_graph(top_logfile_name, time_data):
         top_logfile = open(top_logfile_name, "r")
     except:
         print("top log is not found")
-        return None
+        return
 
     top_data = []
     for i, line in enumerate(top_logfile):
         line = line.rstrip("\n")
         line = line.lstrip()
+        st = time_data["Program start"]
+        gt = time_data["File sent"]
 
         if re.search("^top - ", line):
             now = time.fromisoformat(re.split("\s+", line)[2])
             
         if re.search("protocol", line):
             row = re.split("\s+", line)
-            row.append(now)
+            row[8]= float(row[8])
 
-            if now < time_data["Program start"]:
-                continue 
+            if now < st:
+                continue
 
-            if now > time_data["File sent"]:
+            if now > gt:
                 break
-            
+
+            time_delta = timedelta(hours=now.hour - st.hour, minutes = now.minute - st.minute, seconds = now.second - st.second)
+            row.append(time_delta.seconds)
+
             if len(row) != 13:
                 print("[top data] Format error", file=sys.stderr)
                 continue
-
             top_data.append(row)
     
     top_logfile.close()
 
     top_df = pd.DataFrame(top_data, columns = ["PID", "USER", "PR", "NI", "VIRT", "RES", "SHR", "S", "%CPU", "%MEM", "TIME+", "COMMAND", "TIME"])
     top_fig = px.line(top_df, x="TIME", y="%CPU")
+
+    [x_min, x_max] = [0, max(top_df["TIME"])]
+    [y_min, y_max] = [0, 100]
+
+    top_fig.update_layout(
+        xaxis = dict(
+            title = "Excecution time (s)",
+            range = [x_min, x_max],
+            dtick = 100
+        ),
+        yaxis = dict(
+            title = "CPU usage (%)",
+            range = [y_min, y_max],
+            dtick = 20
+        ),
+        font=dict(
+            family = "Times New Roman",
+            size=36,
+        ),
+    )
+    top_fig.update_traces(
+        mode="lines",
+        line=dict(
+            width=2,
+        )
+    )
     top_fig.show()
 
     if DEBUG:
         print(top_df)
 
-    return top_df
-
-def generate_power_consumption_graph(power_logfile_name, top_data):
+def generate_power_consumption_graph(power_logfile_name, time_data):
     try:
         power_logfile = open(power_logfile_name, "r")
     except:
         print("powerstat log is not found")
         return
-    
-    if top_data == None:
-        return
 
     power_data = []
-    power_start_time = top_data["TIME"][0]
-    power_end_time = top_data["TIME"][-1]
+    #power_start_time = top_data["TIME"][0]
+    #power_end_time = top_data["TIME"][-1]
 
     for line in power_logfile:
-        line = line.rstrip("\n")
+        line = line.rstrip()
+        st = time_data["Program start"]
+        gt = time_data["File sent"]
         
         if re.search("^[0-9]+:[0-9]+:[0-9]+", line):
 
             row = re.split("\s+", line)
             row[0] = time.fromisoformat(row[0])
+            row[12] = float(row[12])
 
-            if row[0] < top_start_time:
+            if row[0] < st:
                 continue
 
-            if row[0] > power_end_time:
+            if row[0] > gt:
                 break
 
-            if len(row) != 10:
+            time_delta = timedelta(hours=row[0].hour - st.hour, minutes = row[0].minute - st.minute, seconds = row[0].second - st.second)
+            row[0] = time_delta.seconds
+            
+            if len(row) != 13:
                 print("[power data] Format error", file=sys.stderr)
                 continue
-
             power_data.append(row)
     
     power_logfile.close()
 
-    power_df = pd.DataFrame(power_data, columns = ["Time", "User", "Nice", "Sys", "Idle", "IO", "Run", "Ctxt/s", "IRQ/s", "Watts"])
+    power_df = pd.DataFrame(power_data, columns = ["Time", "User", "Nice", "Sys", "Idle", "IO", "Run", "Ctxt/s", "IRQ/s", "Fork", "Exec", "Exit", "Watts"])
     power_fig = px.line(power_df, x="Time", y="Watts")
+    
+    [x_min, x_max] = [0, max(power_df["Time"])]
+    [y_min, y_max] = [min(power_df["Watts"]), max(power_df["Watts"])]
+    if y_min - math.floor(y_min) < 0.5:
+        y_min = math.floor(y_min)
+    else:
+        y_min = math.floor(y_min) + 0.5
+    
+    if y_max - math.floor(y_max) > 0.5:
+        y_max = math.ceil(y_max)
+    else:
+        y_max = math.ceil(y_max) - 0.5
+
+    power_fig.update_layout(
+        xaxis = dict(
+            title = "Excecution time (s)",
+            range = [x_min, x_max],
+            dtick = 100
+        ),
+        yaxis = dict(
+            title = "Discharge rate (W/s)",
+            range = [y_min, y_max],
+            dtick = 0.5
+        ),
+        font=dict(
+            family = "Times New Roman",
+            size=36,
+        ),
+    )
+    power_fig.update_traces(
+        mode="lines",
+        line=dict(
+            width=2,
+        )
+    )
     power_fig.show()
 
     if DEBUG:
@@ -134,6 +200,39 @@ def generate_throughput_graph(throughput_logfile_name):
 
     throughput_df = pd.DataFrame(throughput_data, columns = ["transferred", "throughput"])
     throughput_fig = px.line(throughput_df, x="transferred", y="throughput")
+
+    [x_min, x_max] = [0, 100]
+    [y_min, y_max] = [math.floor(min(throughput_df["throughput"])), math.ceil(max(throughput_df["throughput"]))]
+    if y_min % 2 != 0:
+        y_min = y_min - 1
+    if y_max % 2 != 0:
+        y_max = y_max + 1
+
+    throughput_fig.update_layout(
+        xaxis = dict(
+            title = "Percentage of received file (%)",
+            range = [x_min, x_max],
+            dtick = 10
+        ),
+        yaxis = dict(
+            title = "Throughput (Mb/s)",
+            range = [y_min, y_max],
+            dtick = 2
+        ),
+        font=dict(
+            family = "Times New Roman",
+            size=36,
+        ),
+    )
+    throughput_fig.update_traces(
+        mode="lines+markers",
+        marker=dict(
+            size=10,
+        ),
+        line=dict(
+            width=4,
+        )
+    )
     throughput_fig.show()
 
     if DEBUG:
@@ -157,8 +256,8 @@ if __name__ == "__main__":
     throughput_logfile_name = out_basedir + server_outdir + "/" + protocol_name
     
     time_data = get_time_log()
-    top_data = generate_cpu_usage_graph(top_logfile_name, time_data)
-    generate_power_consumption_graph(power_logfile_name, top_data)
+    generate_cpu_usage_graph(top_logfile_name, time_data)
+    generate_power_consumption_graph(power_logfile_name, time_data)
     generate_throughput_graph(throughput_logfile_name)
 
 
