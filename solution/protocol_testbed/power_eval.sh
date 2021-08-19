e46867216f0661b551b445536f1318514f78f237
Tomoya Tanaka <totana@ttu.ee>
2021-08-19 18:34:46 +0300

    Update power_eval.sh

--- solution/protocol_testbed/power_eval.sh
+++ solution/protocol_testbed/power_eval.sh
@@ -1,22 +1,60 @@
 outbasedir=out
-outdir=$outbasedir/$(eval date +"%Y-%m-%d-%H:%M:%S")
-mkdir $outdir
-outdir=`realpath $outdir`
+outdir=$(eval date +"%Y-%m-%d-%H:%M:%S")
+mkdir $outbasedir/$outdir
 
-if [ -e "out/current" ] ; then
-  rm $outbasedir/current
+if [ -e "$outbasedir/current" ] ; then
+  rm -f $outbasedir/current
 fi
 
-echo "ln -s $outdir out/current"
+echo "ln -s $outdir $outbasedir/current"
 ln -s $outdir $outbasedir/current
 
-touch $outdir/powerstat.log
-cat /dev/null > $outdir/powerstat.log
+if [ `ps aux | grep powerstat | grep -v grep | wc -l` -gt 0 ] ; then
+  killall powerstat
+fi
+
+# Pre excecution
+start=`date +%s`
+echo "powerstat -d 10 1 > $outbasedir/$outdir/powerstat.log"
+powerstat -d 10 1 > $outbasedir/$outdir/powerstat.log &
+echo "Pre excecution starts (120s) "
 
 while true
 do
-  echo "powerstat -d 10 1 >> $outdir/powerstat.log"
-  powerstat -d 10 1 >> $outdir/powerstat.log
+  now=`date +%s`
+  runtime=$((now-start))
+  if [ $runtime -ge 120 ] ; then
+    break
+  fi
+done
+echo "Pre excecution done"
+
+killall powerstat
+cat /dev/null > $outbasedir/$outdir/powerstat.log
+
+# Measure power consumption without protocol_testbed
+start=`date +%s`
+echo "powerstat -d 0 1 > $outbasedir/$outdir/powerstat.log"
+powerstat -d 0 1 > $outbasedir/$outdir/powerstat.log &
+echo "Measuring power consumption withought protocol_testbed (60s) "
+
+while true
+do
+  now=`date +%s`
+  runtime=$((now-start))
+  if [ $runtime -ge 60 ] ; then
+    break
+  fi
+done
+echo "Done"
+echo "You can run protocol testbed!"
+
+killall powerstat
+
+while true
+do
+  echo "powerstat -d 0 1 >> $outbasedir/$outdir/powerstat.log"
+  powerstat -d 0 1 >> $outbasedir/$outdir/powerstat.log
 done
 
 
